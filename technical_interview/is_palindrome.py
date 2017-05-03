f829624a96171718652f0fcbdcc292f7785bc520
vprusso <vincentrusso1@gmail.com>
2017-05-03 16:20:34 -0400

    Adding is palindrome'

---
+++ technical_interview/is_palindrome.py
@@ -0,0 +1,25 @@
+"""
+Given a string, write a function to determine if it is a palindrome.
+"""
+
+# racecar
+# madam
+# Dammit I'm mad.
+
+import string
+
+def is_palindrome(s):
+	s = s.lower()
+	s = s.translate(None, string.punctuation)
+	s = s.replace(" ", "")
+
+	return s == s[::-1]
+
+str_1 = "racecar"
+str_2 = "Dammit I'm mad."
+str_3 = "computer"
+
+print(is_palindrome(str_1))
+print(is_palindrome(str_2))
+print(is_palindrome(str_3))
+
