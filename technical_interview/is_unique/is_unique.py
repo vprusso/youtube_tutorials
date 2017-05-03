b1a4c4ee1ce12d5bd377961ec6efa4f99eedea55
vprusso <vincentrusso1@gmail.com>
2017-05-02 22:39:01 -0400

    Adding is_unique code

---
+++ technical_interview/is_unique/is_unique.py
@@ -0,0 +1,18 @@
+"""
+Implement an algorithm to determine if a string has all unique characters.
+"""
+
+# "unique"
+# "bear"
+
+def is_unique(s):
+	return len(s) == len(set(s))
+
+s1 = "unique"
+s2 = "bear"
+
+# False
+print(is_unique(s1)) 
+
+# True
+print(is_unique(s2))
