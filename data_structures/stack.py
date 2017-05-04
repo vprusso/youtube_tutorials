3df73eab5a81b95ac6f4c64cf7c176541217d1ef
root <root@localhost.localdomain>
2017-05-04 18:27:00 -0400

    Adding stack

---
+++ data_structures/stack.py
@@ -0,0 +1,25 @@
+"""
+Stack Data Structure.
+"""
+
+class Stack():
+	def __init__(self):
+		self.items = []
+
+	def push(self, item):
+		self.items.append(item)				
+
+	def pop(self):
+		return self.items.pop()
+
+	def is_empty(self):
+		return self.items == []
+
+	def peek(self):
+		if not self.is_empty():
+			return self.items[-1]
+
+	def get_stack(self):
+		return self.items
+
+
