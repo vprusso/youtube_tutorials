072ce939e4d3dac3901c5f93fb64d4aca4a2372b
vprusso <vincentrusso1@gmail.com>
2017-05-05 13:21:39 -0400

    Adding stack balanced parens

---
+++ data_structures/stack_balanced_parens.py
@@ -0,0 +1,59 @@
+"""
+Use a stack to check whether or not a string 
+has balanced usage of parenthesis. 
+
+Example:
+	(), ()(), (({[]}))  <- Balanced.
+	((), {{{)}], [][]]] <- Not Balanced. 
+
+
+Balanced Example: {[]}
+
+
+Non-Balanced Example: (()
+
+Non-Balanced Example: ))
+"""
+
+from stack import Stack
+
+
+def is_match(p1, p2):
+	if p1 == "(" and p2 == ")":
+		return True
+	elif p1 == "{" and p2 == "}":
+		return True
+	elif p1 == "[" and p2 == "]":
+		return True
+	else:
+		return False
+
+
+def is_paren_balanced(paren_string):
+	s = Stack()
+	is_balanced = True
+	index = 0 
+
+	while index < len(paren_string) and is_balanced:
+		paren = paren_string[index]	
+		if paren in "([{":
+			s.push(paren)
+		else:
+			if s.is_empty():
+				is_balanced = False
+			else:
+				top = s.pop()
+				if not is_match(top, paren):
+					is_balanced = False
+		index += 1
+
+	if s.is_empty() and is_balanced:
+		return True
+	else:
+		return False
+			
+
+print(is_paren_balanced("(((({}))))"))
+
+print(is_paren_balanced("[][]]]"))
+
