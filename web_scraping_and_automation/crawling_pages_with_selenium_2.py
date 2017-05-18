df0bc96a2bb0660a3f0b5ed5d80b83a3a02bbb16
Vincent Russo <vincentrusso1@gmail.com>
2017-05-17 20:24:46 -0400

    Modifying selenium scripts

--- web_scraping_and_automation/crawling_pages_with_selenium_2.py
+++ web_scraping_and_automation/crawling_pages_with_selenium_2.py
@@ -1,11 +1,13 @@
 import csv
 from selenium import webdriver
 
-
-# ------
 MAX_PAGE_NUM = 5
 MAX_PAGE_DIG = 3
 driver = webdriver.Firefox()
+
+with open('results.csv', 'w') as f:
+		f.write("Buyer, Price \n")
+
 for i in range(1, MAX_PAGE_NUM + 1):
 	page_num = (MAX_PAGE_DIG - len(str(i))) * "0" + str(i) 
 	url = "http://econpy.pythonanywhere.com/ex/" + page_num + ".html"
@@ -16,8 +18,7 @@ for i in range(1, MAX_PAGE_NUM + 1):
 	prices = driver.find_elements_by_xpath('//span[@class="item-price"]')
 
 	num_page_items = len(buyers)
-	with open('results.csv', 'w') as f:
-		f.write("Buyer, Price \n")
+	with open('results.csv', 'a') as f:
 		for i in range(num_page_items):
 			f.write(buyers[i].text + "," + prices[i].text + "\n") 
 driver.close()
