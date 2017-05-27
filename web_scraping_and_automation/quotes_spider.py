c07eee2b5eeba4bdc517565390bb244ff077eb17
Vincent Russo <vincentrusso1@gmail.com>
2017-05-27 14:40:49 -0400

    Adding quotes spider

---
+++ web_scraping_and_automation/quotes_spider.py
@@ -0,0 +1,21 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+	name = "quotes"
+	
+	def start_requests(self):
+		urls = [
+			'http://quotes.toscrape.com/page/1/',
+			'http://quotes.toscrape.com/page/2/'
+		]
+		for url in urls:
+			yield scrapy.Request(url=url, callback=self.parse)
+
+	def parse(self, response):
+		page = response.url.split("/")[-2]
+		filename = 'quotes-%s.html' % page
+		with open(filename, 'wb') as f:
+			f.write(response.body)
+		self.log('Saved file %s' % filename)
+
