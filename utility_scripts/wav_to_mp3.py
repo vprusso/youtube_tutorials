47605cf64f6aa4b9db47cc3935b3cb884641a945
Vincent Russo <vincentrusso1@gmail.com>
2017-06-07 18:53:57 -0400

    Adding wav_to_mp3 script

---
+++ utility_scripts/wav_to_mp3.py
@@ -0,0 +1,12 @@
+import os
+import pydub
+import glob
+
+wav_files = glob.glob('./*.wav')
+
+for wav_file in wav_files:
+	mp3_file = os.path.splitext(wav_file)[0] + '.mp3'
+	sound = pydub.AudioSegment.from_wav(wav_file)
+	sound.export(mp3_file, format="mp3")
+	os.remove(wav_file)
+print("Conversion Complete")
