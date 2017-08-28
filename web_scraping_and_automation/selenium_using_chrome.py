ca23ae59ff9d5354447af3164afefdfbf8d889cb
Vincent Russo <vincentrusso1@gmail.com>
2017-08-28 19:17:22 -0400

    Adding more selenium scripts

---
+++ web_scraping_and_automation/selenium_using_chrome.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+# Old way of doing things that works with Firefox
+#driver = webdriver.Firefox()
+#driver.get("http:google.com")
+
+chromedriver = "/home/captainhampton/Downloads/chromedriver"
+driver = webdriver.Chrome(chromedriver)
+driver.get("http:google.com")
