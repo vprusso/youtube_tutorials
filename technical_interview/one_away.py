0635a331b13494e63ae57ccfce61310d0a192b71
Vincent Russo <vincentrusso1@gmail.com>
2017-09-25 20:27:31 -0400

    Adding new scripts

---
+++ technical_interview/one_away.py
@@ -0,0 +1,30 @@
+"""
+1.5
+"""
+# One Away: There are three types of edits that can be performed on strings: insert a character,
+# remove a character, or replace a character. Given two strings, write a function to check if they are
+# one edit (or zero edits) away.
+
+test_str_1 = "pale"
+test_str_2 = "ple"
+
+test_str_3 = "pales"
+test_str_4 = "pale"
+
+test_str_5 = "pale"
+test_str_6 = "bale"
+
+test_str_7 = "pale"
+test_str_8 = "bake"
+
+test_str_9 = "pale"
+test_str_10 = "pale"
+
+def is_one_away(input_str_1, input_str_2):
+	return 0 <= len(list(set(input_str_1) - set(input_str_2))) <= 1
+
+# Should return True
+print(is_one_away(test_str_1, test_str_2))
+
+# Should return False
+print(is_one_away(test_str_7, test_str_8))
---
+++ technical_interview/urlify.py
@@ -0,0 +1,21 @@
+"""
+1.3
+"""
+# URLify: Write a method to replace all spaces in a string with '%20'. You may assume that the string
+# has sufficient space at the end to hold the additional characters, and that you are given the "true"
+# length of the string. (Note: If implementing in Java, please use character array so that you can
+# perform this operation in place.)
+
+input_test_str = "Mr. John Smith"
+input_test_len = len(input_test_str)
+
+def URLify(input_str, input_len):
+	url = ""
+	for i in range(input_len):
+		if input_str[i] == " ":
+			url += "%20"
+		else:
+			url = url + input_str[i]
+	return url
+
+print(URLify(input_test_str, input_test_len))
