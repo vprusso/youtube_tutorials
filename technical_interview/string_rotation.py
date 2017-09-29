ff59c67691a2b84c4e848c50f09268feb7be4b5e
Vincent Russo <vincentrusso1@gmail.com>
2017-09-29 18:19:31 -0400

    Adding to technical interview files

--- technical_interview/string_rotation.py
+++ technical_interview/string_rotation.py
@@ -1,24 +1,24 @@
-# String Rotation: Assume you have a method isSubstring which checks if one word is a substring
-# of another. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one
-# call to isSubstring (e.g. "waterbottle" is a rotation of "erbottlewat")
+# String Rotation: Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 
+# (e.g. "waterbottle" is a rotation of "erbottlewat")
 
 test_rot_str_1 = "waterbottle"
 test_rot_str_2 = "erbottlewat"
 
-test_rot_str_3 = "waterbottle"
+test_rot_str_3 = "watertables"
 test_rot_str_4 = "erbottlewat"
  
+import string
+
 def is_string_rotation(str_1, str_2):
 
-	alphabet = "abcdefghijklmnopqrstuvwxyz"
 	if len(str_1) != len(str_2):
 		return False
 
 	str_1 = str_1.lower()
 	str_2 = str_2.lower()
 
-	dict_1 = dict.fromkeys(list(alphabet), 0)
-	dict_2 = dict.fromkeys(list(alphabet), 0)
+	dict_1 = dict.fromkeys(list(string.ascii_lowercase), 0)
+	dict_2 = dict.fromkeys(list(string.ascii_lowercase), 0)
 
 	for i in range(len(str_1)):
 		dict_1[str_1[i]] += 1
