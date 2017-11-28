5ad7e69024eeda46e2a678c7adde697e02df9509
Vincent Russo <vincent.russo@isara.com>
2017-11-27 20:45:11 -0500

    Adding email scripts

---
+++ utility_scripts/send_email/config.py
@@ -0,0 +1,2 @@
+EMAIL_ADDRESS = ""
+PASSWORD = ""
---
+++ utility_scripts/send_email/send_email.py
@@ -0,0 +1,22 @@
+import smtplib 
+
+import config
+
+def send_email(subject, msg):
+    try:
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login(config.EMAIL_ADDRESS, config.PASSWORD)
+        message = 'Subject: {}\n\n{}'.format(subject, msg)
+        server.sendmail(config.EMAIL_ADDRESS, config.EMAIL_ADDRESS, message)
+        server.quit()
+        print("Success: Email sent!")
+    except:
+        print("Email failed to send.")
+
+subject = "Test subject"
+msg = "Hello there, how are you today?"
+
+send_email(subject, msg)
+
