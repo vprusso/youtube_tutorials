8af11fa3d55fa53c9acf4a6ab8f8caf70a329b6d
vprusso <vincentrusso1@gmail.com>
2017-12-03 20:19:34 -0500

    Adding hello pyautogui

---
+++ web_scraping_and_automation/hello_pyautogui.py
@@ -0,0 +1,11 @@
+import pyautogui, time 
+time.sleep(5)
+pyautogui.click()
+distance = 400
+while distance > 0:
+    pyautogui.dragRel(distance, 0, duration=0.2)
+    distance = distance - 50
+    pyautogui.dragRel(0, distance, duration=0.2)
+    pyautogui.dragRel(-distance, 0, duration=0.2)
+    distance = distance - 50
+    pyautogui.dragRel(0, -distance, duration=0.2)
