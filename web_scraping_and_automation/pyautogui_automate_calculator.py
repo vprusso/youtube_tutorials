50f4cd4b78b6732ccb8687f777dd05adeb418d5a
vprusso <vincentrusso1@gmail.com>
2017-12-09 10:08:40 -0500

    Added pyautogui calculator content

---
+++ web_scraping_and_automation/pyautogui_automate_calculator.py
@@ -0,0 +1,5 @@
+import pyautogui
+pyautogui.click((pyautogui.locateCenterOnScreen('../auxillary_files/1key.png')))
+pyautogui.click((pyautogui.locateCenterOnScreen('../auxillary_files/pluskey.png')))
+pyautogui.click((pyautogui.locateCenterOnScreen('../auxillary_files/1key.png')))
+pyautogui.press('enter')
