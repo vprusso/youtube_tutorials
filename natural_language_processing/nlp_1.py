ca4bae62b2c6ec0b677f1dee7b427bf7c3bb34fa
Vincent Russo <vincent.russo@isara.com>
2018-01-04 11:33:33 -0500

    More changes to NLP python code

--- natural_language_processing/nlp_1.py
+++ natural_language_processing/nlp_1.py
@@ -1,3 +1,9 @@
+# Prior to running this script, you will require Python to be installed on 
+# your machine. If so, you may run the following command via pip:
+#   pip install nltk
+# Once installed, you should be able to follow along with the 
+# remainder of this script. 
+
 import nltk
 
 from nltk.text import Text
@@ -26,18 +32,47 @@ print(len(alice))
 # count the word "the" on each occurrence. 
 print(len(set(alice)))
 
+# Specific Word Count: How many times does a specific word occur 
+# in a text?
+print(alice.count("Alice"))
+
 # Concordance: Shows occurence of word in context of use.
 # We can check where the term "alice" appears in "Alice in Wonderland".
-alice.concordance("alice")
+alice.concordance("Alice")
 
 # Dispersion Plot: Location of where a word is in the text.
 # Example:
 #   Give a visual representation of where the words "Alice", "Rabbit",
 #   "Hatter", and "Queen" appear in "Alice in Wonderland". 
-alice.dispersion_plot(["Alice", "Rabbit", "Hatter", "Queen"])
+#alice.dispersion_plot(["Alice", "Rabbit", "Hatter", "Queen"])
 
 # The word "Alice" is consistently used throughout the entire text, while 
 # the word "Queen" is found closer to the end of the text. This makes sense,
 # since Alice does not encounter the Red Queen until later in the book.
 
+# Frequency Distributions: What are the most frequent words (specifically,
+# tokens), that are used in a given text.
+# Example:
+#   Generate the most frequent tokens in "Alice in Wonderland":
 
+# First, use NLTK to generate a frequncy distribution dictionary object.
+fdist = nltk.FreqDist(alice)
+
+# Second, access the keys of the frequency distribution dictionary.
+# The ordering of the frequency distribution dictionary object 
+# corresponds to how frequently each token is used.
+fdist_keys = fdist.keys()
+
+# What are the top 50 most commond words in "Alice in Wonderland"?
+#fdist.plot(50, cumulative=True)
+
+# We may not obtain too much information on the above plot, since 
+# many of the words on the x-axis are words like "and", "the", "in",
+# etc. These types of common English words are referred to as 
+# stopwords. NLTK provides a method to identify such words.
+
+# TODO: Filter stopwords.
+print(type(fdist))
+fdist_no_stopwords = filter(fdist, nltk.corpus.stopwords.words('english'))
+print(fdist_no_stopwords)
+# TODO: Replot fdist after stopwords filtered out. 
