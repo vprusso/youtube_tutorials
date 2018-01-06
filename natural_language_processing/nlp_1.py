d624b7fc8fd11e0832cc5d822de205c99c6b46dd
vprusso <vincentrusso1@gmail.com>
2018-01-05 19:06:12 -0500

    Adding in NLP to first tutorial

--- natural_language_processing/nlp_1.py
+++ natural_language_processing/nlp_1.py
@@ -1,3 +1,5 @@
+# LucidProgramming -- Natural Language Processing in Python: Part 1
+
 # Prior to running this script, you will require Python to be installed on 
 # your machine. If so, you may run the following command via pip:
 #   pip install nltk
@@ -6,13 +8,12 @@
 
 import nltk
 
-from nltk.text import Text
-
 # Run this command to download all collections to be used for the NLP tutorials:
-#nltk.download()
+nltk.download()
 
 # Now that we've downloaded all the NLTK corpus content, let us go ahead and
 # load in the text from Lewis Carroll's "Alice in Wonderland" via Gutenberg:
+from nltk.text import Text
 alice = Text(nltk.corpus.gutenberg.words('carroll-alice.txt'))
 
 # NLTK also provides other texts from Gutenberg. We can view those by 
@@ -25,6 +26,7 @@ print(nltk.corpus.gutenberg.fileids())
 
 # Word Count: How many words are contained in "Alice in Wonderland"?
 # Note that this includes punctuation as well as traditional words.
+print(type(alice))
 print(len(alice))
 
 # Unique Word Count: How many unique words are present in 
@@ -44,7 +46,7 @@ alice.concordance("Alice")
 # Example:
 #   Give a visual representation of where the words "Alice", "Rabbit",
 #   "Hatter", and "Queen" appear in "Alice in Wonderland". 
-#alice.dispersion_plot(["Alice", "Rabbit", "Hatter", "Queen"])
+alice.dispersion_plot(["Alice", "Rabbit", "Hatter", "Queen"])
 
 # The word "Alice" is consistently used throughout the entire text, while 
 # the word "Queen" is found closer to the end of the text. This makes sense,
@@ -58,21 +60,18 @@ alice.concordance("Alice")
 # First, use NLTK to generate a frequncy distribution dictionary-like object.
 fdist = nltk.FreqDist(alice)
 
-# Second, access the keys of the frequency distribution.
-# The ordering of the frequency distribution object 
-# corresponds to how frequently each token is used.
-fdist_keys = list(fdist.keys())
-
-# What are the top 50 most commond words in "Alice in Wonderland"?
-#fdist.plot(50, cumulative=True)
+# What are the top 50 most common words in "Alice in Wonderland"?
+fdist.plot(50, cumulative=True, title="50 most common tokens in Alice in Wonderland")
 
 # Observe that the x-axis consists of punctuation, which may not 
 # be precisely what we are going for. It is possible to remove this 
 # from the words that we plot by filtering out the punctuation.
+fdist_no_punc = nltk.FreqDist(
+        dict((word, freq) for word, freq in fdist.items() if word.isalpha()))
 
-# TODO:
-punctuation = []
-fdist_no_punc_no_stopwords = nltk.FreqDist(dict((word, freq) for word, freq in fdist.items() if word not in punctuation))
+fdist_no_punc.plot(50,
+                   cumulative=True, 
+                   title="50 most common tokens (no punctuation)")
 
 # This plot gives us a bit more useful information, but it still contains an 
 # awful lot of punctuation that we do not particularly care to see. In a 
@@ -82,19 +81,13 @@ fdist_no_punc_no_stopwords = nltk.FreqDist(dict((word, freq) for word, freq in f
 # many of the words on the x-axis are words like "and", "the", "in",
 # etc. These types of common English words are referred to as 
 # stopwords. NLTK provides a method to identify such words.
-
-#print(type(fdist))
 stopwords = nltk.corpus.stopwords.words('english')
-fdist_no_stopwords = nltk.FreqDist(dict((word, freq) for word, freq in fdist.items() if word not in stopwords))
-#print(fdist_no_stopwords.keys()[0:10])
+fdist_no_punc_no_stopwords = nltk.FreqDist(
+        dict((word, freq) for word, freq in fdist.items() if word not in stopwords and word.isalpha()))
 
 # Replot fdist after stopwords filtered out. 
-fdist_no_stopwords.plot(50, cumulative=True)
-
-
-# Hapaxes: Words that occur exactly once in the text. 
-#print(fdist.hapaxes())
-
-# TODO: Case-insensitive hapaxes?
+fdist_no_punc_no_stopwords.plot(50, 
+                                cumulative=True, 
+                                title="50 most common tokens (no stopwords or punctuation)")
 
 
