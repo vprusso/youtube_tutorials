c0faabdce71e81fef367342cc77e8dd65aa64889
vprusso <vincentrusso1@gmail.com>
2018-01-07 14:35:21 -0500

    Beginning to add part 3 of tutorial series for NLP

---
+++ natural_language_processing/nlp_3.py
@@ -0,0 +1,24 @@
+# LucidProgramming -- Natural Language Processing in Python: Part 3
+
+# Part 3 Blog Post: http://vprusso.github.io/blog/2018/natural-language-processing-python-3/
+# Part 3 YouTube Video:  
+
+# Start off my importing the NLTK module.
+import nltk
+
+# In this tutorial, we will focus on *WordNet*: a lexical 
+# database for the English language. WordNet groups English words 
+# into sets of synonyms, called *synsets*, and provides brief 
+# definitions and usage examples along with recording a number of 
+# relationships among the synonym sets. 
+
+# WordNet can be thought of as an online resource that combines 
+# the functionality of both a dictionary and thesaurus. 
+
+# WordNet may be accessed via NLTK. For more information on WordNet, 
+# consult the following:
+# Wikipedia: https://en.wikipedia.org/wiki/WordNet
+# WordNet Website: https://wordnet.princeton.edu/
+
+
+
