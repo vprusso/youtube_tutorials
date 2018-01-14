0edca4fac61f6873725691b533a98d41ed6fbb97
vprusso <vincentrusso1@gmail.com>
2018-01-14 09:24:04 -0500

    Moving scripts around

--- web_scraping_and_automation/pyautogui_automate_calculator.py
+++
@@ -1,6 +0,0 @@
-# YouTube Video: https://www.youtube.com/watch?v=CXtC8Jzxsvw
-import pyautogui
-pyautogui.click((pyautogui.locateCenterOnScreen('../auxillary_files/1key.png')))
-pyautogui.click((pyautogui.locateCenterOnScreen('../auxillary_files/pluskey.png')))
-pyautogui.click((pyautogui.locateCenterOnScreen('../auxillary_files/1key.png')))
-pyautogui.press('enter')
