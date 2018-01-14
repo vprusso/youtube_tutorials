fd0277235ef5b9852716709489c8a48ecefcd9d5
vprusso <vincentrusso1@gmail.com>
2018-01-14 09:28:42 -0500

    Updating more structure to files

---
+++ utility_scripts/wav_to_mp3/wav_to_mp3.py
@@ -0,0 +1,13 @@
+# YouTube Video: https://www.youtube.com/watch?v=4E7N7W1lUkU
+import os
+import pydub
+import glob
+
+wav_files = glob.glob('./*.wav')
+
+for wav_file in wav_files:
+	mp3_file = os.path.splitext(wav_file)[0] + '.mp3'
+	sound = pydub.AudioSegment.from_wav(wav_file)
+	sound.export(mp3_file, format="mp3")
+	os.remove(wav_file)
+print("Conversion Complete")
