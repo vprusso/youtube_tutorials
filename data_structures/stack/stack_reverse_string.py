7a64d1659e53fcecc5848855bf05694ae26033d0
vprusso <vincentrusso1@gmail.com>
2018-01-20 22:32:19 -0500

    Adding stack reverse string

---
+++ data_structures/stack/stack_reverse_string.py
@@ -0,0 +1,43 @@
+# YouTube Video: 
+
+class Stack():
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def is_empty(self):
+        return self.items == []
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+    def get_stack(self):
+        return self.items
+
+def reverse_string(stack, input_str):
+
+    # Loop through the string and push contents 
+    # character by character onto stack. 
+    for i in range(len(input_str)):
+        stack.push(input_str[i])
+
+    rev_str = ""
+    while not stack.is_empty():
+        rev_str += stack.pop()
+
+    return rev_str
+    
+stack = Stack()    
+input_str = "Hello"
+
+print(reverse_string(stack, input_str))
+
+
+#print(input_str[::-1])
+
