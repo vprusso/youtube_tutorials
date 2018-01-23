34710a6069e5f6ddeab34d1fd94900169629d5ac
vprusso <vincentrusso1@gmail.com>
2018-01-22 22:46:22 -0500

    Adding string length recursion function

---
+++ algorithms/recursion/str_len.py
@@ -0,0 +1,23 @@
+# Given a string, calculate the length of the string. 
+
+input_str = "LucidProgramming"
+
+# Standard Pythonic way:
+# print(len(input_str))
+
+# Iterative length calculation: O(n)
+def iterative_str_len(input_str):
+    input_str_len = 0
+    for i in range(len(input_str)):
+        input_str_len += 1
+    return input_str_len
+
+# Recursive length calculation: O(n)
+def recursive_str_len(input_str):
+    if input_str == '':
+        return 0
+    return 1 + recursive_str_len(input_str[1:])
+
+print(iterative_str_len(input_str))
+print(recursive_str_len(input_str))
+
