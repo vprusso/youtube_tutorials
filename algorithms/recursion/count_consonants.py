51549ff163f82c5c7747d55cbbfd0f072bfb47f2
vprusso <vincentrusso1@gmail.com>
2018-01-23 00:46:58 -0500

    Adding count consonants functions

---
+++ algorithms/recursion/count_consonants.py
@@ -0,0 +1,27 @@
+
+vowels = "aeiou"
+
+input_str = "abc de"
+input_str = "LuCiDPrograMMiNG"
+
+
+def iterative_count_consonants(input_str):
+    consonant_count = 0
+    for i in range(len(input_str)):
+        if input_str[i].lower() not in vowels and input_str[i].isalpha():
+            consonant_count += 1
+    return consonant_count
+
+
+def recursive_count_consonants(input_str):
+    if input_str == '':
+        return 0
+
+    if input_str[0].lower() not in vowels and input_str[0].isalpha():
+        return 1 + recursive_count_consonants(input_str[1:])
+    else:
+        return recursive_count_consonants(input_str[1:])
+
+
+print(iterative_count_consonants(input_str))
+print(recursive_count_consonants(input_str))
