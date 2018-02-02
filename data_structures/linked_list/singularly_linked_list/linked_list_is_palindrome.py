3ec0701bd424a3670a6931922fbd90e0a8a35889
vprusso <vincentrusso1@gmail.com>
2018-02-01 19:54:23 -0500

    Adding is palindrome linked list

--- data_structures/linked_list/singularly_linked_list/linked_list_is_palindrome.py
+++ data_structures/linked_list/singularly_linked_list/linked_list_is_palindrome.py
@@ -293,22 +293,67 @@ class LinkedList:
         else:
             return self.count_occurences_recursive(node.next, data)
 
+    def is_palindrome(self):
+        # Method 1:
+        # s = ""
+        # p = self.head 
+        # while p:
+        #     s += p.data
+        #     p = p.next
+        # return s == s[::-1]
+
+        # Method 2:
+        # p = self.head 
+        # s = []
+        # while p:
+        #     s.append(p.data)
+        #     p = p.next
+        # p = self.head
+        # while p:
+        #     data = s.pop()
+        #     if p.data != data:
+        #         return False
+        #     p = p.next
+        # return True
+
+        # Method 3
+        p = self.head 
+        q = self.head 
+        prev = []
+        
+        i = 0
+        while q:
+            prev.append(q)
+            q = q.next
+            i += 1
+        q = prev[i-1]
+    
+        count = 1
+
+        while count <= i//2 + 1:
+            if prev[-count].data != p.data:
+                return False
+            p = p.next
+            count += 1
+        return True
+
+# Example palindromes:
+# RACECAR, RADAR
+
+# Example non-palindromes:
+# TEST, ABC, HELLO
 
 llist = LinkedList()
-llist.append(1)
-llist.append(2)
-llist.append(3)
-llist.append(4)
-llist.append(5)
-llist.append(6)
+llist.append("R")
+llist.append("A")
+llist.append("D")
+llist.append("A")
+llist.append("R")
 
 llist_2 = LinkedList()
-llist_2.append(1)
-llist_2.append(2)
-llist_2.append(1)
-llist_2.append(3)
-llist_2.append(1)
-llist_2.append(4)
-llist_2.append(1)
-print(llist_2.count_occurences_iterative(1))
-print(llist_2.count_occurences_recursive(llist_2.head, 1))
+llist_2.append("A")
+llist_2.append("B")
+llist_2.append("C")
+
+print(llist.is_palindrome())
+print(llist_2.is_palindrome())
