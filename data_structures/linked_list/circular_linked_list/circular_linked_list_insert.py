7009a59b09b0b4e9356cea143002e4b7266ed439
vprusso <vincentrusso1@gmail.com>
2018-02-03 01:18:02 -0500

    Added circular linked list

---
+++ data_structures/linked_list/circular_linked_list/circular_linked_list_insert.py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self, data):
+        self.data = data 
+        self.next = None
+
+
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None 
+
+    def prepend(self, data):
+        new_node = Node(data)
+        cur = self.head 
+        new_node.next = self.head
+
+        if not self.head:
+            new_node.next = new_node
+        else:
+            while cur.next != self.head:
+                cur = cur.next
+            cur.next = new_node
+        self.head = new_node
+
+    def append(self, data):
+        if not self.head:
+            self.head = Node(data)
+            self.head.next = self.head
+        else:
+            new_node = Node(data)
+            cur = self.head
+            while cur.next != self.head:
+                cur = cur.next
+            cur.next = new_node
+            new_node.next = self.head
+
+    def print_list(self):
+        cur = self.head 
+
+        while cur:
+            print(cur.data)
+            cur = cur.next
+            if cur == self.head:
+                break
+
+cllist = CircularLinkedList()
+cllist.append("C")
+cllist.append("D")
+cllist.prepend("B")
+cllist.prepend("A")
+cllist.print_list()
