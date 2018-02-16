76bf2d554393c34fdc158c215ea77a3b7efb66ba
vprusso <vincentrusso1@gmail.com>
2018-02-15 20:33:31 -0500

    Adding arbit precision arithmetic array

---
+++ data_structures/arrays/arbitrary_precision_arithmetic.py
@@ -0,0 +1,21 @@
+A1 = [1,4,9]
+A2 = [9,9,9]
+
+#s = ''.join(map(str, A))
+#print(int(s) + 1)
+
+def plus_one(A):
+    A[-1] += 1
+    for i in reversed(range(1, len(A))):
+        if A[i] != 10:
+            break
+        A[i] = 0
+        A[i-1] += 1
+    if A[0] == 10:
+        A[0] = 1
+        A.append(0)
+    return A
+
+
+print(plus_one(A1))
+print(plus_one(A2))
