d1f7bd02d6114cdf71806e189927908b809d0130
vprusso <vincentrusso1@gmail.com>
2018-02-18 16:38:41 -0500

    Adding anagram

---
+++ algorithms/string_processing/is_anagram.py
@@ -0,0 +1,33 @@
+s1 = "fairy tales"Rs banned in a place they don’t b
+s2 = "rail safety"
+
+s1 = s1.replace(" ", "").lower()
+s2 = s2.replace(" ", "").lower()
+
+# Requires n log n time (since any comparison 
+# based sorting algorithm requires at least 
+# n log n time to sort).
+#print(sorted(s1) == sorted(s2))
+
+def is_anagram(s1, s2):
+    ht = dict()
+
+    if len(s1) != len(s2):
+        return False
+
+    for i in s1:
+        if i in ht:
+            ht[i] += 1
+        else:
+            ht[i] = 1
+    for i in s2:
+        if i in ht:
+            ht[i] -= 1
+        else:
+            ht[i] = 1
+    for i in ht:
+        if ht[i] != 0:
+            return False
+    return True
+
+print(is_anagram(s1, s2))
