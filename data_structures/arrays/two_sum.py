30e70996577e88175cf95825778ec9eb4c8f4664
vprusso <vincentrusso1@gmail.com>
2018-02-19 22:52:43 -0500

    Adding two sum

--- data_structures/arrays/two_sum.py
+++ data_structures/arrays/two_sum.py
@@ -1,3 +1,6 @@
+# Given an array of integers, return the two numbers such that they add up to a specific target. 
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
 A = [-2, 1, 2, 4, 7, 11]
 target = 13
 
