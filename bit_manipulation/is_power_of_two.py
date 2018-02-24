93565f5769adc8e135c09aa387f37f0e6fd6bd77
vprusso <vincentrusso1@gmail.com>
2018-02-24 14:34:15 -0500

    adding binary manip functions

--- bit_manipulation/is_power_of_two.py
+++ bit_manipulation/is_power_of_two.py
@@ -4,20 +4,24 @@ Given a positive integer N, check if N is a power of 2.
 
 # Method 1: Use Log_2
 import math
+
+
 def is_power_of_two_1(n):
-	return math.log(n,2) == math.floor(math.log(n,2))
+    return math.log(n, 2) == math.floor(math.log(n, 2))
+
 
 # Method 2: Without Python math library
 def is_power_of_two_2(n):
-	if n == 0: 
-		return False
-	else:
-		while n != 1:
-			if n%2 != 0:
-				return False
-			else:
-				n = n/2
-	return True
+    if n == 0: 
+        return False
+    else:
+        while n != 1:
+            if n % 2 != 0:
+                return False
+            else:
+                n = n/2
+    return True
+
 
 # Method 3: Without looping
 def is_power_of_two_3(n):
@@ -25,6 +29,7 @@ def is_power_of_two_3(n):
         return False
     else:
         return n and not(n & (n-1))
+
 
 n = 2
 print(is_power_of_two_1(n))
