8344695dc89c126b5ea45e3984a95608c77c4342
Vincent Russo <vincent.russo@isara.com>
2018-02-28 15:08:11 -0500

    Adding more go

---
+++ go/14_variadic_functions.go
@@ -0,0 +1,11 @@
+// p14. Variadic Functions
+package main
+
+import "fmt"
+
+func main() {
+    // function that takes an arbitrary number of args.
+
+    // Println is an example:
+    fmt.Println("this", "is", "an", "example", "of", "a", "variadic", "function")
+}
