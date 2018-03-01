764742980b74b768594a4251935c428689920c57
vprusso <vincentrusso1@gmail.com>
2018-03-01 18:30:55 -0500

    Adding to go

--- go/14_variadic_functions.go
+++ go/14_variadic_functions.go
@@ -3,9 +3,26 @@ package main
 
 import "fmt"
 
+func mult(nums ...int)int {
+    accum_mult := 1
+    for _, num := range nums {
+        // accum_mult = accum_mult * num
+        // shorthand:
+        accum_mult *= num
+    }
+    return accum_mult
+}
+
 func main() {
     // function that takes an arbitrary number of args.
 
     // Println is an example:
     fmt.Println("this", "is", "an", "example", "of", "a", "variadic", "function")
+    
+    // Call mult function with as many variables as necessary
+    fmt.Println(mult(1,2,3,4,5))
+    
+    // Variadic functions can also be applied to slices:
+    nums := []int{1, 2, 3, 4}
+    fmt.Println(mult(nums...))
 }
