b359eded551e23a51af9d506b6cb57b29880a4af
vprusso <vincentrusso1@gmail.com>
2018-03-01 23:08:43 -0500

    Adding more go files

--- go/15_closures.go
+++ go/15_closures.go
@@ -4,5 +4,44 @@ package main
 
 import "fmt"
 
+func say_hello(msg string){
+    fmt.Println(msg)
+}
+
+func return_anoyn_func() func(string) {
+    // return an anonymous function
+    return func(msg string){
+        fmt.Print(msg)
+    }
+}
+
+func int_seq() func() int {
+    i := 0
+    return func() int {
+        i++
+        return i
+    }
+}
+
 func main(){
+
+    say_hello("Hello")
+
+    // anonymous function declared:
+    func(msg string){
+        fmt.Println(msg)
+    }("Hello Anonymous")
+
+    //print_func := return_anoyn_func()
+    //print_func("Hello returned from anonymous")
+
+    next_int := int_seq()
+
+    fmt.Println(next_int())
+    fmt.Println(next_int())
+
+    next_int2 := int_seq()
+
+    fmt.Println(next_int2())
+
 }
--- go/16_recursion.go
+++ go/16_recursion.go
@@ -1,8 +1,19 @@
 // p.16 Recursion
 
+// n! = n * n-1 * n-2 * .. * 1
+// 5! = 5 * 4 * 3 * 2 * 1
+
 package main
 
 import "fmt"
 
+func fact(n int)int {
+    if n <= 1 {
+        return 1
+    }
+    return n * fact(n-1)
+}
+
 func main(){
+    fmt.Println(fact(5))
 }
