60eae88cd0e0743010ea449797ae969a8df2a0d3
Vincent Russo <vincent.russo@isara.com>
2018-03-02 13:00:02 -0500

    adding methods

--- go/19_methods.go
+++ go/19_methods.go
@@ -4,5 +4,32 @@ package main
 
 import "fmt"
 
+type rect struct {
+    width, height int
+}
+
+// method defined for value reciever type.
+func (r rect) area() int {
+    return r.width + r.height
+}
+
+// method defined for pointer reciever type.
+func (r *rect) perim() int {
+    return 2*r.width * 2*r.height
+}
+
 func main(){
+
+    r := rect{width: 10, height: 5}
+
+    // call to method for value reciever type.
+    fmt.Println("area:", r.area())
+    fmt.Println("perim:", r.perim())
+
+    r_ptr := &r
+
+    // call to method for pointer reciever type.
+    fmt.Println("area:", r_ptr.area())
+    fmt.Println("perim:", r_ptr.perim())
+
 }
