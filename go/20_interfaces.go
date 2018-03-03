4ade00717d398eb432bbe46478d761a164f8c5b1
Vincent Russo <vincent.russo@isara.com>
2018-03-02 20:23:02 -0500

    Adding go files

--- go/20_interfaces.go
+++ go/20_interfaces.go
@@ -2,7 +2,53 @@
 
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "math"
+)
+
+// interface for shapes:
+type geometry interface {
+    area() float64
+    perim() float64
+}
+
+type rect struct {
+    width, height float64
+}
+
+type circle struct {
+    radius float64
+}
+
+// implement all methods for interface:
+func (r rect) area() float64 {
+    return r.width * r.height
+}
+
+func (r rect) perim() float64 {
+    return 2*r.width + 2*r.height
+}
+
+func (c circle) area() float64 {
+    return math.Pi * c.radius * c.radius
+}
+
+func (c circle) perim() float64 {
+    return 2 * math.Pi * c.radius
+}
+
+func measure(g geometry) {
+    fmt.Println(g)
+    fmt.Println(g.area())
+    fmt.Println(g.perim())
+}
 
 func main(){
+
+    r := rect{width: 5, height: 10}
+    c := circle{radius: 5}
+
+    measure(r)
+    measure(c)
 }
--- go/21_errors.go
+++ go/21_errors.go
@@ -2,7 +2,35 @@
 
 package main
 
-import "fmt"
+import (
+    "errors"
+    "fmt"
+)
+
+func errorExample(arg string) (string, error) {
+    if arg == "No" {
+        return "", errors.New("Input No is not valid")
+    }
+    return "Input " + arg + " is valid", nil
+}
+
+// Custom types as errors (use built-in Error() function)
+type argError struct {
+    arg string
+    prob string
+}
+
+func (e *argError) Error() string {
+    return fmt.Printf("%s - %s", e.arg, e.prob)
+}
+
+func customErrorExample(arg string) (string, error) {
+    if arg == "No" {
+        return "", &argError{arg, "Input No is not valid"}
+    }
+    return "Input " + arg + " is valid", nil
+}
+
 
 func main(){
 }
