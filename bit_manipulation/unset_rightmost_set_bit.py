2d6bfda317e1ee45a7fb8c829a16b7f045a4f62c
vprusso <vincentrusso1@gmail.com>
2018-04-13 11:17:21 -0400

    Updating setting most right set bit

---
+++ bit_manipulation/unset_rightmost_set_bit.py
@@ -0,0 +1,52 @@
+"""
+Write a program  that takes an integer and
+unsets the right most bit in the binary representation 
+of that integer
+
+For instance, the binary representation of 7 is:
+    111
+
+The least significant bit is the bit on the far right
+of the binary representation and the most significant
+bit is the bit on the far left. We order the bits as
+
+b2, b1, b0
+1   1   1
+
+For our function, if we unset the right-most bit,
+we should obtain the binary representation:
+    1 1 0
+"""
+
+
+"""
+Observation:
+
+We can combine some of the ideas present in the video 
+one how to unset the nth bit:
+    XXX
+
+Example: Unset the right-most bit:
+
+        0 1 0 1 0 1 1 1     (x)
+    &   0 1 0 1 0 1 1 0     (x-1)
+        ---------------
+        0 1 0 1 0 1 1 0
+
+Example: Unset the right-most bit:
+
+        0 1 0 1 1 0 0 0     (x)
+    &   0 1 0 1 0 1 1 1     (x-1)
+        ---------------
+        0 1 0 1 0 0 0 0
+"""
+
+
+def unset_rightmost_set_bit(x: int):
+    return x & (x-1)
+
+
+# Unset rightmost bit of binary representation of 7:
+y = unset_rightmost_set_bit(7)
+print(y)
+
