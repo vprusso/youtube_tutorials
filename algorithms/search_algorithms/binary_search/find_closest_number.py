71018113dea24f33bec53fa05a82bb74c8cc42f2
vprusso <vincentrusso1@gmail.com>
2018-07-08 17:21:28 -0400

    Update find closest number

--- algorithms/search_algorithms/binary_search/find_closest_number.py
+++ algorithms/search_algorithms/binary_search/find_closest_number.py
@@ -22,7 +22,7 @@ A = [2, 5, 6, 7, 8, 8, 9]
 
 
 def find_closest_num(A, target):
-    max_diff = float("inf")
+    min_diff = float("inf")
     low = 0
     high = len(A) - 1
     closest_num = None
@@ -40,28 +40,29 @@ def find_closest_num(A, target):
         # Ensure you do not read beyond the bounds
         # of the list.
         if mid+1 < len(A):
-            max_diff_right = abs(A[mid + 1] - target)
-        max_diff_left = abs(A[mid - 1] - target)
+            min_diff_right = abs(A[mid + 1] - target)
+        if mid > 0:
+            min_diff_left = abs(A[mid - 1] - target)
 
         # Check if the absolute value between left
         # and right elements are smaller than any
         # seen prior.
-        if max_diff_left < max_diff:
-            max_diff = max_diff_left
+        if min_diff_left < min_diff:
+            min_diff = min_diff_left
             closest_num = A[mid - 1]
 
-        if max_diff_right < max_diff:
-            max_diff = max_diff_right
+        if min_diff_right < min_diff:
+            min_diff = min_diff_right
             closest_num = A[mid + 1]
-        
+
         # Move the mid-point appropriately as is done
         # via binary search.
         if A[mid] < target:
             low = mid + 1
         elif A[mid] > target:
             high = mid - 1
-        # If the element itself is the target, the closest 
-        # number to it is itself. Return the number. 
+        # If the element itself is the target, the closest
+        # number to it is itself. Return the number.
         else:
             return A[mid]
     return closest_num
