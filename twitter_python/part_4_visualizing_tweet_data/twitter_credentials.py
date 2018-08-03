687ce0b172b94e5416a1c66824b11291b8b51036
vprusso <vincentrusso1@gmail.com>
2018-08-03 13:03:27 -0400

    Adding more twitter python code

---
+++ twitter_python/part_4_visualizing_tweet_data/twitter_credentials.py
@@ -0,0 +1,6 @@
+# YouTube Video: https://www.youtube.com/watch?v=rhBZqEWsZU4
+# Variables that contains the user credentials to access Twitter API 
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
