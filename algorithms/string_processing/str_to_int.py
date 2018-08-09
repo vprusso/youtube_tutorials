50206123bbd1fe0a2f3a298aa59c6befe3bd40be
vprusso <vincentrusso1@gmail.com>
2018-08-08 23:21:42 -0400

    str to int function

--- algorithms/string_processing/str_to_int.py
+++ algorithms/string_processing/str_to_int.py
@@ -1,23 +1,44 @@
 """
-You are given some numeric string as input. Conver the string you are
+You are given some numeric string as input. Convert the string you are
 given to an integer. Do not make use of the built-in "int" function.
 
 Example:
     "123" -> 123
+    "-12332" -> -12332
     "554" -> 554
     etc.
 """
 
 
 def str_to_int(input_str):
-    s = 0
-    for i in range(len(input_str)):
+
+    output_int = 0
+
+    if input_str[0] == '-':
+        start_idx = 1
+        is_negative = True
+    else:
+        start_idx = 0
+        is_negative = False
+
+    for i in range(start_idx, len(input_str)):
         place = 10**(len(input_str) - (i+1))
         digit = ord(input_str[i]) - ord('0')
-        s += digit * place
-    return s
+        output_int += place * digit
+
+    if is_negative:
+        return -1 * output_int
+    else:
+        return output_int
 
 
-str_to_int("123")
-str_to_int("3283")
 
+s = "554"
+x = str_to_int(s)
+print(type(x))
+
+s = "123"
+print(str_to_int(s))
+
+s = "-123"
+print(str_to_int(s))
