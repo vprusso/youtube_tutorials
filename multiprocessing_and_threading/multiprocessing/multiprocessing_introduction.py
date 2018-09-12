488c39430c31e209ae17b6c0cd632d36d1ccc2e4
vprusso <vincentrusso1@gmail.com>
2018-09-12 14:25:33 -0400

    Adding multiprocessing introduction

---
+++ multiprocessing_and_threading/multiprocessing/multiprocessing_introduction.py
@@ -0,0 +1,39 @@
+# YouTube Link:
+
+# Introduction an simple example of the multiprocessing module in Python.
+# We show how to simply apply this module to a function that takes a number
+# and squares it.
+
+import os
+
+from multiprocessing import Process
+
+
+def square(number):
+    """The function squares whatever number it is provided."""
+    result = number * 2
+    proc_id = os.getpid()
+    print("Process ID: " + str(proc_id))
+    print("The number " + str(number) + " squares to " + str(result) + ".")
+
+
+if __name__ == '__main__':
+
+    # The processes list will store each call we make to "square" and the
+    # numbers list contains the numbers we loop through and call the
+    # "square" function on."
+    processes = []
+    numbers = [1, 2, 3, 4, 5]
+
+    # Loop through the list of numbers, call the "square" function,
+    # and store and start each call to "square".
+    for i, number in enumerate(numbers):
+        process = Process(target=square, args=(number,))
+        processes.append(process)
+        process.start()
+
+    # Wait for Python process to end before starting the
+    # next process.
+    for process in processes:
+        process.join()
+
