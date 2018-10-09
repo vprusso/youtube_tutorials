c83b0218a93ac380ca5b2b7a8c91b986d2362733
vprusso <vincentrusso1@gmail.com>
2018-10-09 14:39:31 -0400

    Adding to multiprocessing

--- multiprocessing_and_threading/multiprocessing/multiprocessing_locks.py
+++ multiprocessing_and_threading/multiprocessing/multiprocessing_locks.py
@@ -7,31 +7,86 @@
 # More on locks:
 # https://en.wikipedia.org/wiki/Lock_(computer_science)
 
-from multiprocessing import Process, Lock
+import time
+from multiprocessing import Process, Lock, Value
 
 
-def info(item, lock):
-    """Outputs the item passed in."""
+def add_500_no_mp(total):
+    for i in range(100):
+        time.sleep(0.01)
+        total += 5
+    return total
 
-    # Acquire a lock prior to using it.
-    lock.acquire()
 
-    # If the lock is released, print the item.
-    # Otherwise, release the lock.
-    try:
-        print(item)
-    finally:
+def sub_500_no_mp(total):
+    for i in range(100):
+        time.sleep(0.01)
+        total -= 5
+    return total
+
+
+def add_500_no_lock(total):
+    for i in range(100):
+        time.sleep(0.01)
+        total.value += 5
+
+
+def sub_500_no_lock(total):
+    for i in range(100):
+        time.sleep(0.01)
+        total.value -= 5
+
+
+def add_500_lock(total, lock):
+    for i in range(100):
+        time.sleep(0.01)
+        lock.acquire()
+        total.value += 5
         lock.release()
 
 
-if __name__ == '__main__':
-    lock = Lock()
-    items = ['Lucid', 'Programming', 'Tutorials']
+def sub_500_lock(total, lock):
+    for i in range(100):
+        time.sleep(0.01)
+        lock.acquire()
+        total.value -= 5
+        lock.release()
 
-    # Loop through all of the items in the above list and create
-    # a process for each element in the list.
-    for item in items:
-        # Passing and using the lock allows us to ensure that
-        # the next process will wait until the lock is released.
-        p = Process(target=info, args=(item, lock))
-        p.start()
+
+#if __name__ == '__main__':
+#
+#    total = 500
+#    print(total)
+#    total = add_500_no_mp(total)
+#    print(total)
+#    total = sub_500_no_mp(total)
+#    print(total)
+
+#if __name__ == '__main__':
+#
+#    total = Value('i', 500)
+#    add_proc = Process(target=add_500_no_lock, args=(total,))
+#    sub_proc = Process(target=sub_500_no_lock, args=(total,))
+#
+#    add_proc.start()
+#    sub_proc.start()
+#
+#    add_proc.join()
+#    sub_proc.join()
+#    print(total.value)
+
+if __name__ == '__main__':
+
+    total = Value('i', 500)
+    lock = Lock()
+    add_proc = Process(target=add_500_lock, args=(total, lock))
+    sub_proc = Process(target=sub_500_lock, args=(total, lock))
+
+    add_proc.start()
+    sub_proc.start()
+
+    add_proc.join()
+    sub_proc.join()
+    print(total.value)
+
+
