4d89c80baab809ce01f0323e2fd742aa3557e39d
vprusso <vincentrusso1@gmail.com>
2018-11-21 07:40:25 -0500

    Adding more string content

--- algorithms/string_processing/spreadsheet_decode.py
+++ algorithms/string_processing/spreadsheet_decode.py
@@ -1,4 +1,17 @@
+# YouTube Link:
+"""
+Implement a function that converts an integer to the spreadsheet
+column ID.
+
+Example:
+    Input: 1 -> A
+    Input: 27 -> AA
+    Input: 702 -> ZZ
+"""
+
+
 def spreadsheet_decode_column(col_num):
+    """Decodes a column number into a column id."""
     col_str = ""
     while col_num > 0:
         col_num, remainder = divmod(col_num - 1, 26)
@@ -6,4 +19,11 @@ def spreadsheet_decode_column(col_num):
     return col_str
 
 
+# A
+print(spreadsheet_decode_column(1))
+
+# AA
+print(spreadsheet_decode_column(27))
+
+# ZZ
 print(spreadsheet_decode_column(702))
