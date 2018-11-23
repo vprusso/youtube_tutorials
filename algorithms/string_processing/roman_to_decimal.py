fa11daa22641d1b700d2942f28a5aeaf29aeb475
vprusso <vincentrusso1@gmail.com>
2018-11-22 23:20:36 -0500

    Adding roman to decimal python file

---
+++ algorithms/string_processing/roman_to_decimal.py
@@ -0,0 +1,19 @@
+# YouTube Link:
+"""
+Write a function that converts Roman numerals to decimal numbers.
+
+Example:
+    I -> 1
+    II -> 2
+    V -> 5
+    IX -> 9
+    CM -> 900
+    etc.
+"""
+
+
+def roman_numeral_to_decimal(roman_numeral):
+    """Converts a roman numeral to a decimal number."""
+    pass
+
+
