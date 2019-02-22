4c334df3678a8618537b4c82ff3d90240567d77f
vprusso <vincentrusso1@gmail.com>
2019-02-22 08:35:15 -0500

    Adding to part 2

--- machine_learning/iris_classification/part_2.py
+++ machine_learning/iris_classification/part_2.py
@@ -26,7 +26,7 @@ iris = load_iris()
 # To do this, we can split our data up into two sets;
 # a training and testing set. The training set will be
 # what our model uses to learn, and the test set will be
-# the remaining set that assesses whether the model is 
+# the remaining set that assesses whether the model is
 # able to accurately predict the outcome of the measurements
 # from this set.
 
@@ -37,5 +37,22 @@ iris = load_iris()
 # to use as a starting point.
 
 # Split our dataset into training and testing sets.
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(iris['data'], iris['target'], random_state=0)
 
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots(3, 3, figsize=(15, 15))
+plt.suptitle("iris_pairplot")
 
+for i in range(3):
+    for j in range(3):
+        ax[i, j].scatter(x_train[:, j], x_train[:, i + 1], c=y_train, s=60)
+        ax[i, j].set_xticks(())
+        ax[i, j].set_yticks(())
+        if i == 2:
+            ax[i, j].set_xlabel(iris['feature_names'][j])
+        if j == 0:
+            ax[i, j].set_ylabel(iris['feature_names'][i + 1])
+        if j > i:
+            ax[i, j].set_visible(False)
+plt.show()
