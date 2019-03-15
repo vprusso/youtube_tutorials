37ee2aa880ae91d3e395c683ea92dc2106dfc1d0
vprusso <vincentrusso1@gmail.com>
2019-03-14 20:25:47 -0400

    Adding to iris classification

--- machine_learning/iris_classification/part_2.py
+++ machine_learning/iris_classification/part_2.py
@@ -8,39 +8,12 @@
 # length, width, etc. are the "features" that determine the classification of a
 # given iris flower.
 
-# Will be used to split the iris data set into train/test sets:
-from sklearn.model_selection import train_test_split
-
 # Will be used to generate plots:
 import matplotlib.pyplot as plt
-
 
 # Import the iris dataset as provided by the sklearn Python module:
 from sklearn.datasets import load_iris
 iris = load_iris()
-
-# Goal: Built machine learning model from the iris data set that can predict
-# the species of a new set of measurements.
-
-# In order to determine how well our model performs, we need to run it on data
-# it has not seen before, that is, we need to run it on a new set of
-# measurements and see where our model categorizes this new item.
-
-# To do this, we can split our data up into two sets; a training and testing
-# set. The training set will be what our model uses to learn, and the test set
-# will be the remaining set that assesses whether the model is able to
-# accurately predict the outcome of the measurements from this set.
-
-# We will be using a 75/25 split for train/test respectively. That is, we will
-# be training our model on 75% of our data, and then testing on the remaining
-# 25%. What split percentage you use is up to you, but a 75/25 split is a
-# reasonable rule to use as a starting point.
-
-# Split our dataset into training and testing sets.
-X_train, X_test, y_train, y_test = train_test_split(iris['data'],
-                                                    iris['target'],
-                                                    random_state=0)
-
 
 # Store the features of the iris data set into a "features" variable.
 features = iris.data.T
--- machine_learning/iris_classification/part_3.py
+++ machine_learning/iris_classification/part_3.py
@@ -1,10 +1,10 @@
-# Iris Classification Model: Machine learning model that will allow us to 
+# Iris Classification Model: Machine learning model that will allow us to
 # classify species of iris flowers. This application will introduce many
 # rudimentary features and concepts of machine learning and is a good use case
 # for these types of models.
 
 # Use case: Botanist wants to determine the species of an iris flower based on
-# characteristics of that flower. For instance attributes including petal 
+# characteristics of that flower. For instance attributes including petal
 # length, width, etc. are the "features" that determine the classification
 # of a given iris flower.
 
@@ -26,6 +26,21 @@ iris = load_iris()
 # point and determine which predication has the majority class among
 # the neightbors. We will start by considering one neighbor for now.
 
+# In order to determine how well our model performs, we need to run it on data
+# it has not seen before, that is, we need to run it on a new set of
+# measurements and see where our model categorizes this new item.
+
+# To do this, we can split our data up into two sets; a training and testing
+# set. The training set will be what our model uses to learn, and the test set
+# will be the remaining set that assesses whether the model is able to
+# accurately predict the outcome of the measurements from this set.
+
+# We will be using a 75/25 split for train/test respectively. That is, we will
+# be training our model on 75% of our data, and then testing on the remaining
+# 25%. What split percentage you use is up to you, but a 75/25 split is a
+# reasonable rule to use as a starting point.
+
+# Split our dataset into training and testing sets.
 X_train, X_test, y_train, y_test = train_test_split(iris['data'], iris['target'], random_state=0)
 knn = KNeighborsClassifier(n_neighbors=1)
 
@@ -47,6 +62,4 @@ print(iris['target_names'][prediction])
 # Overall predication score for how likely our model was able to correctly
 # predict the species of flower on the test set.
 print(knn.score(X_test, y_test))
-
-
 
