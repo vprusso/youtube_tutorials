811fca182493f421e12c43005361de35b979bc0c
vprusso <vincentrusso1@gmail.com>
2019-04-10 11:33:37 -0400

    Adding text classification ml code

---
+++ machine_learning/text_classification/imdb.py
@@ -0,0 +1,55 @@
+from __future__ import absolute_import, division, print_function
+
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+
+imdb = keras.datasets.imdb
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+
+#print(f"Training entries {len(train_data)}. Labels: {len(train_labels)}")
+
+#print(len(train_data[0]), len(train_data[1]))
+
+word_index = imdb.get_word_index()
+
+word_index = {k: (v+3) for k,v in word_index.items()}
+word_index["<PAD>"] = 0
+word_index["<START>"] = 1
+word_index["<UNK>"] = 2
+word_index["<UNUSED>"] = 3
+
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+def decode_review(text):
+    return ' '.join([reverse_word_index.get(i, "?") for i in text])
+
+print(train_data[0])
+print(decode_review(train_data[0]))
+
+train_data = keras.preprocessing.sequence.pad_sequences(train_data,
+                                                        value=word_index["<PAD>"],
+                                                        padding="post",
+                                                        maxlen=256)
+
+test_data = keras.preprocessing.sequence.pad_sequences(test_data,
+                                                       value=word_index["<PAD>"],
+                                                       padding="post",
+                                                       maxlen=256)
+
+
+print(len(train_data[0]), len(train_data[1]))
+
+print(train_data[0])
+
+vocab_size = 10000
+
+model = keras.Sequential()
+model.add(keras.layers.Embedding(vocab_size, 16))
+model.add(keras.layers.GlobalAveragePooling1D())
+model.add(keras.layers.Dense(16, activation=tf.nn.relu))
+model.add(keras.layers.Dense(1, activation=tf.nn.sigmoid))
+
+model.summary()
