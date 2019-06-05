fb58098060d2b085a0e815c372cd0ef31832c343
vprusso <vincentrusso1@gmail.com>
2019-06-05 11:04:29 -0400

    Fixed edge case in two-sum

--- data_structures/arrays/two_sum.py
+++ data_structures/arrays/two_sum.py
@@ -6,7 +6,6 @@
 A = [-2, 1, 2, 4, 7, 11]
 target = 13
 
-
 # Time Complexity: O(n^2)
 # Space Complexity: O(1)
 def two_sum_brute_force(A, target):
@@ -36,7 +35,7 @@ def two_sum_hash_table(A, target):
 def two_sum(A, target):
     i = 0
     j = len(A) - 1
-    while i <= j:
+    while i < j:
         if A[i] + A[j] == target:
             print(A[i], A[j])
             return True
