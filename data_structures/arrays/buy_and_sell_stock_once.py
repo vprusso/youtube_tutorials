6738586c8835875eafb2bf526c9d62deec4ea8d9
mmohamedali <sugaal@gmail.com>
2019-07-15 22:38:38 -0400

    Fixed Runtime Error that was causing the code to not run in the Leetcode editor because we weren't checking if the length of array is less than 2

--- data_structures/arrays/buy_and_sell_stock_once.py
+++ data_structures/arrays/buy_and_sell_stock_once.py
@@ -16,16 +16,18 @@ def buy_and_sell_once(A):
 # Time Complexity: O(n)
 # Space Complexity: O(1)
 def buy_and_sell_stock_once(prices):
-    max_profit = 0.0
-    min_price = float('inf')
-
-    for price in prices:
-        min_price = min(min_price, price)
-
-        compare_profit = price - min_price
-
-        max_profit = max(max_profit, compare_profit)
+    # Check if the prices array is less than 2
+    if len(prices) < 2:
+        return 0
         
+    min_price = prices[0]
+    max_profit = 0
+    
+    for price in prices: 
+       	min_price = min(min_price, price)
+        compare_profit = price - min_price
+        max_profit = max(max_profit, compare_profit)
+    
     return max_profit
 
 
